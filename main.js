6bddb550372eb8c82b69aae0167117ab21a01189
Alexander Hed <alexander.hed@gmail.com>
2022-01-17 15:16:55 +0100

    initialization script module added

---
+++ main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";(function(){const t=document.getElementById("content");return{initialization:function(){const n=document.createElement("h1");n.innerText="Awseom Restaurant",t.appendChild(n)}}})().initialization()})();
