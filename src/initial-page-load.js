3adb9de0d889ccb5ac1872040b49908d796da684
Alexander Hed <alexander.hed@gmail.com>
2022-01-17 16:18:09 +0100

    page skeleton bundled with webpack

--- src/initial-page-load.js
+++ src/initial-page-load.js
@@ -1,11 +1,21 @@
+import Picture from './restaurant.jpeg'
 
 const initialPageLoad = (function () {
     const _content = document.getElementById("content")
 
     function initialization() {
-        const _title = document.createElement('h1')
-        _title.innerText = "Awseom Restaurant";
+        const _title = document.createElement('h1');
+        _title.innerText = "Awsome Restaurant";
         _content.appendChild(_title);
+
+        const _copy = document.createElement('p');
+        _copy.innerText = "Swing by and feel the schwung in the tummy"
+        _content.appendChild(_copy);
+
+        const _resturantPicture = new Image();
+        _resturantPicture.src = Picture;
+        _content.appendChild(_resturantPicture);
+
     }
 
     return {initialization};
